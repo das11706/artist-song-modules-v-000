65b0f56690cb250eb60defcafb48527f67534df3
Dan Suarez <suarez.daniel101@gmail.com>
2019-06-17 03:46:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable
-  include Findable
+  extend Findable
 
   attr_accessor :name
   attr_reader :artist
